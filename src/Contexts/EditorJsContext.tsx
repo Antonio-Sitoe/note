e99041a2dbb293c3af9ebb240f90cc9fa1c99a29
Antonio Sitoe <antoniositoehl@gmail.com>
2023-03-12 11:25:02 +0200

    adicionado

---
+++ src/Contexts/EditorJsContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useCallback, useRef } from "react";
+
+export const EditorJsContext = createContext({});
+export function EditorJsStorage({ children }) {
+  const editorJS = useRef(null);
+  const handleInitialize = useCallback((instance) => {
+    editorJS.current = instance;
+  }, []);
+
+  const handleSave = useCallback(async () => {
+    const savedData = await editorJS.current.save();
+    console.log(savedData);
+  }, []);
+
+  const handleClear = useCallback(async () => {
+    await editorJS.current.clear();
+  }, []);
+  value = { handleInitialize, handleSave, handleClear };
+  return (
+    <EditorJsContext.Provider value={value}>
+      {children}
+    </EditorJsContext.Provider>
+  );
+}
+
+export default EditorJsContext;
